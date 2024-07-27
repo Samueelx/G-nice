df11db0c9160e0e41fc3f12135d4bd753a1caa3b
Samueelx <gsammygitau@gmail.com>
2024-07-27 19:14:05 +0300

    Fixed eslint bug.

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 import CircularImage from "../components/specific/CircularImage";
 import HomeButton from "../components/specific/HomeButton";
@@ -16,67 +16,94 @@ type fetchedData = {
     transform?: string;
   };
   size: string;
-}
+};
 
 const Homepage: React.FC = () => {
   const [people, setPeople] = useState<fetchedData[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
   const hasFetched = useRef(false);
+  const styles = useMemo(() => {
+    const styles = [
+      { top: "15%", left: "20%" },
+      { top: "90%", left: "60%" },
+      { top: "50%", left: "50%", transform: "translate(-50%, -50%)" },
+      { top: "70%", left: "20%" },
+      { top: "60%", left: "80%" },
+    ];
+    return styles;
+  }, []);
   useEffect(() => {
-    if(hasFetched.current){
+    if (hasFetched.current) {
       return;
     }
     const fetchData = async () => {
       try {
         const response = await fetchPersons();
-        const fetchedPeople = response!.data.results.map((user: Person, index: number) => ({
-          id: user.id.value,
-          email: user.email,
-          src: user.picture.large,
-          style: styles[index],
-          size: getRandomSize()
-        }))
+        const fetchedPeople = response!.data.results.map(
+          (user: Person, index: number) => ({
+            id: user.id.value,
+            email: user.email,
+            src: user.picture.large,
+            style: styles[index],
+            size: getRandomSize(),
+          })
+        );
         console.log("Response: ", response!.data.results);
         setPeople(fetchedPeople);
       } catch (error) {
-        setError('Error Fetching data');
+        setError("Error Fetching data");
       } finally {
         setLoading(false);
       }
     };
 
     fetchData();
-  }, [hasFetched]);
+  }, [hasFetched, styles]);
   const getRandomSize = () => {
-    const sizes = ['w-16 h-16', 'w-20 h-20', 'w-24 h-24 w-32 h-32'];
+    const sizes = ["w-16 h-16", "w-20 h-20", "w-24 h-24 w-32 h-32"];
     return sizes[Math.floor(Math.random() * sizes.length)];
   };
-  const styles = [
-    { top: '15%', left: '20%' },
-    { top: '90%', left: '60%' },
-    { top: '50%', left: '50%', transform: 'translate(-50%, -50%)' },
-    { top: '70%', left: '20%' },
-    { top: '60%', left: '80%' },
-  ];
 
-  if(loading)
-    return <div className="flex justify-center items-center min-h-screen bg-[#EA7AF4]">Loading...</div>
+  if (loading)
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-[#EA7AF4]">
+        Loading...
+      </div>
+    );
   if (error)
-    return <div className="flex justify-center items-center min-h-screen bg-[#EA7AF4]">{error}</div>
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-[#EA7AF4]">
+        {error}
+      </div>
+    );
   return (
     <div className="relative min-h-screen bg-gradient-to-b from-[#EA7AF4] to-[#8A2BE2] flex flex-col items-center gap-4">
       <div className="relative h-3/4 py-16 flex items-start justify-center gap-2 min-h-3.5 w-4/6">
         {people.map((person) => (
-          <CircularImage key={person.email} src={person.src} size={person.size} style={person.style} />
+          <CircularImage
+            key={person.email}
+            src={person.src}
+            size={person.size}
+            style={person.style}
+          />
         ))}
       </div>
       <div className="relative text-center text-white md:p-4">
-        <h1 className="text-2xl font-bold md:text-3xl">The Best Social App To Make New Friends!</h1>
-        <p className="md:text-2xl">Find people with the same interests as you</p>
+        <h1 className="text-2xl font-bold md:text-3xl">
+          The Best Social App To Make New Friends!
+        </h1>
+        <p className="md:text-2xl">
+          Find people with the same interests as you
+        </p>
       </div>
       <div className="flex flex-col gap-6">
-        <HomeButton color={`text-[#290628]`} border={`border`} text="Sign Up" bgColor={`bg-white`}/>
+        <HomeButton
+          color={`text-[#290628]`}
+          border={`border`}
+          text="Sign Up"
+          bgColor={`bg-white`}
+        />
         <LoginButton />
       </div>
     </div>
