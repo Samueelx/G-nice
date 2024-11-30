12467fc73ca1d1a844b51c10f5e1d10863b3618d
Samueelx <gsammygitau@gmail.com>
2024-11-30 23:55:47 +0300

    try to fix routing bug

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
     },
     {
         path: '/feeds',
-        element: <PrivateRoute><LandingPage/></PrivateRoute>
+        element: <PublicRoute><LandingPage/></PublicRoute>
     }
 ]);
 
