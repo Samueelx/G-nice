127ac5bf27ea56b9bb29844593f48f2f6c7c4250
Samueelx <gsammygitau@gmail.com>
2025-01-03 16:38:40 +0300

    design the header section of the feeds page.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,8 +1,41 @@
+import { Menu } from "lucide-react";
+
 const LandingPage: React.FC = () => {
   return (
-    <>
-      <h1>Landing Page</h1>
-    </>
+    <main>
+      <header className="p-4 pb-2 md:p-8">
+        <nav>
+          <div className="avatar flex items-center justify-between pb-4">
+            <div className="w-24">
+              <img
+                className="rounded-full shadow-md hover:cursor-pointer w-3/5 md:w-full"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+            <div className="w-24 ">
+              <img
+                className="rounded-full shadow-md hover:cursor-pointer w-3/5 md:w-full"
+                src="/g-icon.svg"
+                alt="G Icon"
+              />
+            </div>
+            <div>
+              <Menu
+                size={45}
+                style={{
+                  boxShadow: "3px 3px red .5rem 0 .4em olive",
+                  cursor: "pointer",
+                }}
+              />
+            </div>
+          </div>
+          <hr className="w-screen md:hidden" />
+        </nav>
+      </header>
+      <section>
+        <h2 className="text-2xl font-bold px-2">Feeds</h2>
+      </section>
+    </main>
   );
 };
 
