3afc6cc4a5c1680748ebc53bb192e3bc0e2307c5
Samueelx <gsammygitau@gmail.com>
2025-05-26 10:40:25 +0300

    feat: Add support for setEventDetails so you can delegate all event-related messages (including details)

--- src/features/events/handleEventSocketMessages.ts
+++ src/features/events/handleEventSocketMessages.ts
@@ -1,16 +1,18 @@
 import { AppDispatch } from "@/store/store";
-import { setEvents, setEventsError } from "./eventsSlice";
+import { setEvents, setEventsError, setEventDetails } from "./eventsSlice";
 
 export const handleEventSocketMessages = (data: any, dispatch: AppDispatch) => {
-    switch(data.type){
-        case 'EVENTS_LIST':
-            dispatch(setEvents(data.payload));
-            break;
-        case 'EVENTS_ERROR':
-            dispatch(setEventsError(data.payload));
-            break;
-
-        default:
-            console.warn('Unhandled event message type:', data.type);
-    }
+  switch (data.type) {
+    case 'EVENTS_LIST':
+      dispatch(setEvents(data.payload));
+      break;
+    case 'EVENT_DETAILS_DATA':
+      dispatch(setEventDetails(data.payload));
+      break;
+    case 'EVENTS_ERROR':
+      dispatch(setEventsError(data.payload));
+      break;
+    default:
+      console.warn('[WebSocket] Unhandled event message type:', data.type);
+  }
 };
